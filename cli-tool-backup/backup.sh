7b2922b600661abcc6db55705a30110ed3d4b0b7
XaviFelix <xavierfelix74@gmail.com>
2025-04-08 19:08:49 -0700

    core behavior implemented

--- cli-tool-backup/backup.sh
+++ cli-tool-backup/backup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# TODO: If dev label is already mounted then ignore mount_label call
+#
+# TODO: Pass an argument to this command that lets you back up a specific resource
+#       NOTE: Look into using variable length args for arbritrary args by user
+
+#TODO: Add a prompt asking user if they're sure they want to proceed with backup
+
+#TODO: unmount after backing up
 function unmount_label() {
   if [[ -n "$DEVICE_PATH" ]]; then
     echo "Unmounting $DEVICE_PATH..."
@@ -21,11 +29,14 @@ function mount_label() {
 }
 
 #TODO: Test this
-function backup_resources() {
+function backup_currentdir() {
   local LABEL_PATH="/run/media/$USER/$LABEL"
 
   if [[ -e "$LABEL_PATH" ]]; then
-    echo "The path exist: $LABEL_PATH"
+    # echo "The path exist: $LABEL_PATH"
+    cp -r * "$LABEL_PATH"
+    echo "Copying resources to $LABEL..."
+    echo "Finished copying resources to $LABEL"
   else
     echo "The path $LABEL_PATH does not exist"
   fi
@@ -49,7 +60,7 @@ DEVICE_PATH=$(lsblk -o LABEL,PATH | grep "^$LABEL" | awk '{print $2}')
 if [[ -e "$DEVICE_PATH" ]]; then
   echo "$DEVICE_PATH is a valid block device"
   mount_label
-  backup_resources
+  backup_currentdir
 else
   echo "$LABEL Not found"
   exit 1
