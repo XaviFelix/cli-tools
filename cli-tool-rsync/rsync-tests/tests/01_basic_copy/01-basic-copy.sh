b5a80819d3630f9f7708c9d1a8604ffabab0eff6
XaviFelix <xavierfelix74@gmail.com>
2025-04-24 13:13:19 -0700

    rsync tests

--- cli-tool-rsync/rsync-tests/tests/01_basic_copy/01-basic-copy.sh
+++ cli-tool-rsync/rsync-tests/tests/01_basic_copy/01-basic-copy.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 #TODO: Make sure concept is never zero
 CONCEPT="${PWD##*/}"
 source "$(dirname "$0")/../../lib.sh"
-set_up_source_tree "$CONCEPT"
+# set_up_source_tree "$CONCEPT"
 
 #NOTE: assumes that these exist because they are supposed
 #      to be created by 'set_up_source_tree' function
@@ -14,7 +14,7 @@ dst="$PWD/dst"
 
 if [[ -d "$src" && -d "$dst" ]]; then
   #NOTE: a mirror perfect copy from src to dst
-  rsync -av --delete "$src/" "$dst/"
+  rsync -av --delete $src/{bin,docs,.hidden_dir,.hidden_file} "$dst/"
   echo "Done"
 else
   echo "Error finding directories:"
